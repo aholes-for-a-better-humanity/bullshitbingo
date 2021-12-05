624eb95baca68d2f73b31e9c780b933181addadc
Valéry Fouques <valery.fouques@free.fr>
2021-12-05 17:08:23 +0100

    Preload bounding boxes

--- BBGame.go
+++ BBGame.go
@@ -11,13 +11,13 @@ func runBBGame(UI *ui.GridUI) {
 	Hydrate(UI)
 }
 
-
 func Hydrate(UI *ui.GridUI) {
 	rand.Shuffle(len(words), func(i, j int) { words[i], words[j] = words[j], words[i] })
 	UI.Columns = 5
 	UI.Lines = 5
-	UI.Widgets = make([]ui.Widget, 25)
-	for i := 0; i < 25; i++ {
+	UI.Widgets = make([]ui.Widget, UI.Columns*UI.Lines)
+	for i := 0; i < len(UI.Widgets); i++ {
 		UI.Widgets[i] = &widgets.Text{Msg: words[i], Pad: 4}
+		UI.Widgets[i].PreloadBbox()
 	}
 }
