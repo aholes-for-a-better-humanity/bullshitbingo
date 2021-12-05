e91c4d6fb03d42a56c5fdfebfbd22c64de0ada2b
Atrament <alberic.atama@gmail.com>
2021-12-05 16:51:27 +0100

    Resize words on screen

--- ui/widgets/text.go
+++ ui/widgets/text.go
@@ -1,21 +1,21 @@
 package widgets
 
 import (
+	"image"
 	"image/color"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/aholes-for-a-better-humanity/bullshitbingo/ui"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
-
-	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 )
 
 var (
-	mplusNormalFont font.Face
+	fontsL []font.Face
 )
 
 func init() {
@@ -23,16 +23,24 @@ func init() {
 	if err != nil {
 		log.Fatal().Err(err).Msg(``)
 	}
-
 	const dpi = 72
-	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
-		Size:    24,
-		DPI:     dpi,
-		Hinting: font.HintingFull,
-	})
-	if err != nil {
-		log.Fatal().Err(err).Msg(``)
+	var fsizes []float64
+	for i := 100.00; i > 0; i -= 0.25 {
+		fsizes = append(fsizes, i)
 	}
+	fontsL = make([]font.Face, len(fsizes))
+	for i, fsize := range fsizes {
+		var err error
+		fontsL[i], err = opentype.NewFace(tt, &opentype.FaceOptions{
+			Size:    fsize,
+			DPI:     dpi,
+			Hinting: font.HintingFull,
+		})
+		if err != nil {
+			log.Fatal().Err(err).Msg(``)
+		}
+	}
+
 }
 
 var _ ui.Widget = &Text{}
@@ -40,13 +48,23 @@ var _ ui.Widget = &Text{}
 type Text struct {
 	Msg    string
 	Bckgrd color.RGBA
+	Pad    int // pixel value f the padding in p
+	Fsize  float64
 }
 
 func (t *Text) Update() error { return nil }
 func (t *Text) Draw(screen *ebiten.Image) {
 	screen.Fill(t.Bckgrd)
-	textDims := text.BoundString(mplusNormalFont, t.Msg)
-	text.Draw(screen, t.Msg, mplusNormalFont,
+	var textDims image.Rectangle
+	var fontFace font.Face
+	for i := 0; i < len(fontsL); i++ {
+		fontFace = fontsL[i]
+		textDims = text.BoundString(fontFace, t.Msg)
+		if textDims.Dx() < screen.Bounds().Dx()-t.Pad*2 && textDims.Dy() < screen.Bounds().Dy()-t.Pad*2 {
+			break
+		}
+	}
+	text.Draw(screen, t.Msg, fontFace,
 		screen.Bounds().Min.X+screen.Bounds().Dx()/2-textDims.Dx()/2,
 		screen.Bounds().Min.Y+screen.Bounds().Dy()/2+textDims.Dy()/2, // origin is on the text baseline (bottom of letters)
 		color.White)
